65103a9ba6248c4ada1326818792eb40e3298b9e
Steven <scbccb100@gmail.com>
2015-02-27 14:46:03 -1000

    Test Run 1
    Ran the raw data file through the script to make sure it works. Need to
    create a script that will take the completed data and convert it to a
    temperature or flow rate value.

--- NI_Extract_Utility/driver.py
+++ NI_Extract_Utility/driver.py
@@ -2,7 +2,7 @@ import NISignalExpressUtility
 import os
 from DataFiles import __data__
 from ConvertedCSVs import __converted__
-from ReshapedCSVs import __reshaped__   
+from ReshapedCSVs import __reshaped__
 from CalibratedCSVs import __calibrated__
 
 data_location = os.path.dirname(os.path.abspath(__data__.__file__))
@@ -19,9 +19,11 @@ data_folders_locations = []
 for folder_name in os.listdir(data_location):
     if ".py" not in folder_name:
         data_folders_locations.append(os.path.join(data_location, folder_name))
+#        print data_folders_locations
 
 
 for data_folders in data_folders_locations:
+    print data_folders
     NIExtract = NISignalExpressUtility.NISignalExpressUtility(\
         data_folders, converted_location,\
         reshaped_location, calibrated_location)
