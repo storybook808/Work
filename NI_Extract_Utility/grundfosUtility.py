509ea56149641956c4866db4ddde206661ef516c
Steven <scbccb100@gmail.com>
2015-03-03 13:58:42 -1000

    Documentation & Clean Up

--- NI_Extract_Utility/grundfosUtility.py
+++ NI_Extract_Utility/grundfosUtility.py
@@ -5,39 +5,86 @@ from CalibratedCSVs import __calibrated__
 from Output import __output__
 
 """
+Engineering Notes:
+
 [#1] VFS 1-20
-========================
+================================================================================
 ai0 : temperature sensor
 ai1 : flow sensor
 
 Input File
-==========================================
+================================================================================
 Datetime Stamp, Sensor ID, Value, Comments
 
 Output File
-===========================================================
+================================================================================
 Datetime Stamp, Sensor ID, Value, Comments, Converted Value
 """
 
+#Edit following paths upon folder changes
+#===============================================================================
+#Input folder
+input_location = os.path.dirname(os.path.abspath(__calibrated__.__file__))
+#Output folder
+output_location = os.path.dirname(os.path.abspath(__output__.__file__))
+#===============================================================================
+
+
 def convert_flow(voltage):
+    """
+    Input(s):
+    @voltage: (int/float) The output voltage from the Grundfos sensor
+        
+    Returns: (float) The flow rate in l/min
+
+    Notes:
+    Computes the l/min:
+        flow_rate = 6.6522 x output_voltage - 2.5396
+
+    This equation was derived from experiments to measure the accuracy of the sensor
+
+    The ideal equation provided by the company is:
+        flow_rate = (187/30) x voltage - (109/60)
+    """
     return 6.6522*float(voltage)-2.5396
 
 def convert_temperature(voltage):
+    """
+    Input(s):
+    @voltage: (int/float) The output voltage from the Grundfos sensor
+        
+    Returns: (float) The temperature in Fahrenheit
+
+    Notes:
+    Computes the ideal temperature:
+        temperature = (100/3) x output_voltage - (100/6)
+    """
     return celsius_fahrenheit((100.0/3.0)*float(voltage)-(100.0/6.0))
 
 def celsius_fahrenheit(temperature):
+    """
+    Input(s):
+    @temperature: (int/float) Temperature in Celsius
+        
+    Returns: (float) The temperature in Fahrenheit
+
+    Notes:
+    Converts from Celsius to Fahrenheit
+        F = (9/5) x C + 32
+    """
     return (9.0/5.0)*temperature+32.0
 
-calibrated_location = os.path.dirname(os.path.abspath(__calibrated__.__file__))
-output_location = os.path.dirname(os.path.abspath(__output__.__file__))
-
-print "Calibrated:\t", calibrated_location
-print "Output:\t\t", output_location
-
+#Print target locations
+print "Input:\t", input_location
+print "Output:\t", output_location
+#Print the start message
 print "\nStarting conversion...\n"
-
-#Tranverse through every item in current calibrated data location
-for item in os.listdir(calibrated_location):
+#Initialize error count
+error_count = 0
+#Initialize data point count
+data_count = 0
+#Tranverse through every item in current input data location
+for item in os.listdir(input_location):
     #Parse the file name for the extension
     item_target = item.split(".")
     #Target the extension of the file name
@@ -65,7 +112,7 @@ for item in os.listdir(calibrated_location):
             #Open output file with a CSV writer
             output_writer = csv.writer(output_file)
             #Generate a path to the input file
-            item_path = os.path.join(calibrated_location, item)
+            item_path = os.path.join(input_location, item)
             #Open the input file
             with open(item_path, "r") as item_file:
                 #Open input file with a CSV reader
@@ -78,11 +125,27 @@ for item in os.listdir(calibrated_location):
                     output_buffer = []
                     #Copy the contains of the current row to the buffer
                     output_buffer = copy.deepcopy(row)
+
+                    #Edit following list when adding more sensors
+                    #===========================================================
                     #[#1] VFS 1-20 temperature sensor
                     if row[1] == "ai0":
                         output_buffer.append(convert_temperature(row[2]))
                     #[#1] VFS 1-20 Flow sensor
-                    else:
+                    elif row[1] == "ai1":
                         output_buffer.append(convert_flow(row[2]))
+                    #No sensor information can be found, skip to the next row
+                    else:
+                        #Increment error count
+                        error_count = error_count + 1
+                        continue
+                    #===========================================================
+
+                    #Increment data point count
+                    data_count = data_count + 1
                     #Write new row into output file
                     output_writer.writerow(output_buffer)
+#Conversion complete, report data to the user
+print "\nConversion completed...\n"
+print "Number of data points: ", data_count
+print "Number of errors: ", error_count
